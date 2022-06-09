ba96195546468401c0f1e13871804a2490024ec6
Sara Heath <saramaheath@gmail.com>
2022-06-09 14:11:56 -0500

    added get route to app.py, added dynamice jinja templates to survey_start.html

--- app.py
+++ app.py
@@ -7,3 +7,17 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.get('/')
+def home_page():
+    """takes user to home page"""
+
+    questions = survey.questions
+    title = survey.title
+    instructions = survey.instructions
+
+    return render_template("survey_start.html",
+    title=title,
+    instructions=instructions)
